8bbfe0f4871a7e5272c0960d13f1237b6c53b108
eggplants <w10776e8w@yahoo.co.jp>
2021-09-23 02:05:38 +0900

    fix: revert original get_kana and rename new get_kana->get_kana_for_sentence

--- alkana/main.py
+++ alkana/main.py
@@ -11,13 +11,11 @@ def get_kana(word):
     # type: (str) -> str | None
     """
     Returns the reading of `word` as katakana.
-
     Parameters
     ----------
     word : str
         The alphabetical string you want to get the katakana reading.
         The case of string is ignored.
-
     Returns
     ----------
     reading : str
@@ -25,10 +23,33 @@ def get_kana(word):
         `None` if reading is not found.
     """
     try:
+        return data[word.lower()]
+    except KeyError:
+        return None
+
+
+def get_kana_for_sentence(sentence):
+    # type: (str) -> str | None
+    """
+    Returns the reading of `word` as katakana.
+
+    Parameters
+    ----------
+    sentence : str
+        A sentence with mixed letters of the alphabet.
+        The case of string is ignored.
+
+    Returns
+    ----------
+    reading : str
+        A sentence in which all alphabetic strings in `sentence` are replaced with its reading.
+        `None` if reading is not found.
+    """
+    try:
         return sub(
             r'[a-z]+',
             lambda s: data[s.group(0)],
-            str(word).lower())
+            str(sentence).lower())
 
     except KeyError:
         return None
